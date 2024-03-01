7c84edb9ac7f35c6389e68a6743e0d424d0caed2
wangfeng <wangfengxy@sina.cn>
2024-03-02 06:01:58 +0800

    抽象部分常用的go-chart用法

---
+++ plot/charts.go
@@ -0,0 +1,49 @@
+package plot
+
+import (
+	"bytes"
+	"fmt"
+	"gitee.com/quant1x/gox/api"
+	"github.com/wcharczuk/go-chart/v2"
+	"os"
+)
+
+const (
+	DotWidth = 3 // 点的宽度
+)
+
+func CreateChart() chart.Chart {
+	font, _ := GetDefaultFont()
+	font.Bounds(1)
+	lineChartStyle := chart.Style{
+		Padding: chart.Box{
+			//Top:  10,
+			Left: 50,
+		},
+	}
+	graph := chart.Chart{
+		Font:       font,
+		Background: lineChartStyle,
+	}
+	return graph
+}
+
+func AddSeries(graph chart.Chart, series ...chart.Series) chart.Chart {
+	graph.Series = append(graph.Series, series...)
+	return graph
+}
+
+func Render(graph chart.Chart, code string) {
+	graph.Elements = []chart.Renderable{chart.LegendThin(&graph)}
+	buffer := bytes.NewBuffer([]byte{})
+	err := graph.Render(chart.PNG, buffer)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = os.WriteFile(code+".png", buffer.Bytes(), api.CACHE_FILE_MODE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
