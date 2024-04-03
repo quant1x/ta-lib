c34e823088ecce8ed5d13b24918402158a70ae5a
wangfeng <wangfengxy@sina.cn>
2024-04-03 16:36:57 +0800

    新增数据样本结构

--- blas/analysis.go
+++ blas/analysis.go
@@ -1,60 +1,6 @@
 package blas
 
-import "gitee.com/quant1x/engine/datasource/base"
-
-// Analysis 分析接口
-type Analysis interface {
-	// Normalize 归一化处理(Initialize)
-	Normalize() error
-	// Process 加工处理
-	Process() error
-	// At 获取数据, 返回即时值, 最高值, 最低值
-	At(n int) (current, high, low float64)
-	// Match 匹配
-	Match()
-}
-
-// Pattern 模式, 形态
-type Pattern interface {
-	Match(waves Waves) Pattern
-}
-
-type DataSample interface {
-	Len() int              // 数据长度
-	Current(n int) float64 // 当前值
-	High(n int) float64    // 最高
-	Low(n int) float64     // 最低
-	Time(n int) string     // 时间
-}
-
-type KLineSample struct {
-	data []base.KLine
-}
-
-// LoadKLineSample 加载K线样本
-func LoadKLineSample(data []base.KLine) KLineSample {
-	return KLineSample{data: data}
-}
-
-func (k KLineSample) Len() int {
-	return len(k.data)
-}
-
-func (k KLineSample) Current(n int) float64 {
-	return k.data[n].Close
-}
-
-func (k KLineSample) High(n int) float64 {
-	return k.data[n].High
-}
-
-func (k KLineSample) Low(n int) float64 {
-	return k.data[n].Low
-}
-
-func (k KLineSample) Time(n int) string {
-	return k.data[n].Date
-}
+import "github.com/wcharczuk/go-chart/v2"
 
 // TendencyDirection 交易方向
 type TendencyDirection = uint8
@@ -82,4 +28,22 @@ const (
 type OperationalSignal struct {
 	Signal TendencyDirection // 操作信号
 	Digits int               // 保留小数点几位
+}
+
+// Analysis 分析接口
+type Analysis interface {
+	// Normalize 归一化处理(Initialize)
+	Normalize() error
+	// Process 加工处理
+	Process() error
+	// At 获取数据, 返回即时值, 最高值, 最低值
+	At(n int) (current, high, low float64)
+	// Match 匹配
+	Match()
+}
+
+// Pattern 模式, 形态
+type Pattern interface {
+	//Match(waves Waves) Pattern
+	ExportSeries(sample DataSample) []chart.Series
 }
---
+++ blas/samples.go
@@ -0,0 +1,112 @@
+package blas
+
+import (
+	"gitee.com/quant1x/engine/datasource/base"
+	"gitee.com/quant1x/num"
+	"gitee.com/quant1x/ta-lib/plot"
+)
+
+// DataSample 数据样本
+type DataSample interface {
+	Time(n int) string            // 时间
+	Len() int                     // 数据长度
+	Current(n int) float64        // 当前值
+	High(n int) float64           // 最高
+	Low(n int) float64            // 最低
+	Volume(n int) float64         // 量
+	Chart(name string) plot.Chart // 图表
+}
+
+type KLineSample struct {
+	data []base.KLine
+}
+
+// LoadKLineSample 加载K线样本
+func LoadKLineSample(data []base.KLine) KLineSample {
+	return KLineSample{data: data}
+}
+
+func (k KLineSample) Time(n int) string {
+	return k.data[n].Date
+}
+
+func (k KLineSample) Len() int {
+	return len(k.data)
+}
+
+func (k KLineSample) Current(n int) float64 {
+	return k.data[n].Close
+}
+
+func (k KLineSample) High(n int) float64 {
+	return k.data[n].High
+}
+
+func (k KLineSample) Low(n int) float64 {
+	return k.data[n].Low
+}
+
+func (k KLineSample) Volume(n int) float64 {
+	return k.data[n].Volume
+}
+
+func (k KLineSample) Chart(name string) plot.Chart {
+	xAxisFormat := func(v any) string {
+		f := v.(float64)
+		idx := int(f)
+		//xAxis := DATE[idx]
+		x := k.Time(idx)
+		return x
+	}
+	rows := k.Len()
+	xAxis := num.Range[float64](rows)
+	yPrimaryName := "PRICE"
+	yPrimary := make([]float64, rows)
+	ySecondaryName := "VOLUME"
+	ySecondary := make([]float64, rows)
+	//ticks := make([]plot.Tick, plot.XTickMax)
+	ticks := make([]plot.Tick, rows)
+	for i := 0; i < rows; i++ {
+		yPrimary[i] = k.Current(i)
+		ySecondary[i] = k.Volume(i)
+		//ti := int(float64(i) / float64(rows) * plot.XTickMax)
+		//ticks[ti] = plot.Tick{Value: float64(i), Label: k.Time(i)}
+		ticks[i] = plot.Tick{Value: float64(i), Label: k.Time(i)}
+	}
+	graph := plot.NewChart()
+	graph.XAxis = plot.XAxis{
+		Name:           name,
+		TickPosition:   plot.TickPositionUnset,
+		ValueFormatter: xAxisFormat,
+		TickStyle: plot.Style{
+			TextRotationDegrees: 45.0,
+		},
+		Ticks: ticks,
+	}
+	graph.YAxis = plot.YAxis{
+		Name: "股价",
+		GridMajorStyle: plot.Style{
+			StrokeColor:     plot.ColorAlternateLightGray.WithAlpha(200),
+			StrokeWidth:     1.0,
+			StrokeDashArray: plot.DashedLine,
+		},
+	}
+	graph.Series = []plot.Series{
+		plot.ContinuousSeries{
+			Name:    yPrimaryName,
+			XValues: xAxis,
+			YValues: yPrimary,
+			//XValueFormatter: xAxisFormat,
+			Style: plot.Style{StrokeColor: plot.ColorBlack},
+		},
+		plot.ContinuousSeries{
+			Name:    ySecondaryName,
+			YAxis:   plot.YAxisSecondary,
+			XValues: xAxis,
+			YValues: ySecondary,
+			//XValueFormatter: xAxisFormat,
+			Style: plot.Style{StrokeColor: plot.ColorAlternateGray, StrokeDashArray: plot.DashedLine},
+		},
+	}
+	return graph
+}
