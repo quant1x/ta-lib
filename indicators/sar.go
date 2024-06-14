8eb611a35c8db676f3837b85560ba13f448d0d95
wangfeng <wangfengxy@sina.cn>
2024-06-14 13:42:56 +0800

    新增SAR指标计算方法

---
+++ indicators/sar.go
@@ -0,0 +1,82 @@
+package indicators
+
+type FeatureSar struct {
+	Pos  int     // 坐标位置
+	Bull bool    // 当前多空
+	Af   float64 // 加速因子(Acceleration Factor)
+	Ep   float64 // 极值点(Extreme Point)
+	Sar  float64 // SAR[Pos]
+}
+
+// https://baike.baidu.com/item/SAR%E6%8C%87%E6%A0%87
+func tdx_sar(firstIsBull bool, highs, lows []float64, accelerationFactor, accelerationFactorLimit float64) []FeatureSar {
+	length := len(highs)
+	data := make([]FeatureSar, length)
+
+	// 第一个bar
+	data[0].Pos = 0
+	data[0].Bull = firstIsBull
+	// 加速因子
+	data[0].Af = accelerationFactor
+	// 极值点
+	//extremePoint := highs[0]
+	data[0].Ep = highs[data[0].Pos]
+	data[0].Sar = lows[data[0].Pos]
+	for i := 1; i < length; i++ {
+		prevSar := data[i-1]
+		data[i] = prevSar
+		current := &data[i]
+		current.Pos = i
+		current.Bull = prevSar.Bull
+		// 1. 初次赋值
+		if data[i-1].Bull {
+			// 多头
+			if highs[i] > prevSar.Ep {
+				// 创新高
+				current.Ep = highs[i]
+				current.Af = min(prevSar.Af+accelerationFactor, accelerationFactorLimit)
+			}
+		} else {
+			// 空头
+			if lows[i] < prevSar.Ep {
+				// 创新低
+				current.Ep = lows[i]
+				current.Af = min(prevSar.Af+accelerationFactor, accelerationFactorLimit)
+			}
+		}
+		// 2. 计算SAR
+		current.Sar = prevSar.Sar + current.Af*(current.Ep-prevSar.Sar)
+		// 3. 修正SAR
+		if prevSar.Bull {
+			current.Sar = max(prevSar.Sar, min(current.Sar, lows[i], lows[i-1]))
+		} else {
+			current.Sar = min(prevSar.Sar, max(current.Sar, highs[i], highs[i-1]))
+		}
+		// 4. 判断多空
+		if prevSar.Bull {
+			// 多
+			if lows[i] < current.Sar {
+				// 向下跌破, 转空
+				current.Bull = false
+				current.Ep = lows[i]
+				current.Af = accelerationFactor
+				if highs[i-1] == prevSar.Ep {
+					// 紧邻即高点
+					current.Sar = prevSar.Ep
+				} else {
+					current.Sar = prevSar.Ep + current.Af*(current.Ep-prevSar.Ep)
+				}
+			}
+		} else {
+			// 空
+			if highs[i] > current.Sar {
+				// 向上突破, 转多
+				current.Bull = true
+				current.Ep = highs[i]
+				current.Af = accelerationFactor
+				current.Sar = min(lows[i], lows[i-1])
+			}
+		}
+	}
+	return data
+}
---
+++ indicators/sar_test.go
@@ -0,0 +1,31 @@
+package indicators
+
+import (
+	"fmt"
+	"gitee.com/quant1x/engine/datasource/base"
+	"gitee.com/quant1x/exchange"
+	"gitee.com/quant1x/pandas"
+	"testing"
+)
+
+func Test_tdx_sar(t *testing.T) {
+	code := "600171"
+	date := "2024-06-13"
+	code = exchange.CorrectSecurityCode(code)
+	date = exchange.FixTradeDate(date)
+	list := base.CheckoutKLines(code, date)
+	rows := len(list)
+	high := make([]float64, rows)
+	low := make([]float64, rows)
+	firstIsBull := false
+	for i, v := range list {
+		if i == 0 {
+			firstIsBull = v.Close > v.Open
+		}
+		high[i] = v.High
+		low[i] = v.Low
+	}
+	data := tdx_sar(firstIsBull, high, low, 0.02, 0.20)
+	df := pandas.LoadStructs(data)
+	fmt.Println(df)
+}
