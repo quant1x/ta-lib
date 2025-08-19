59edbbbd3245a1a71e0893dc64f093a541182cb4
wangfeng <wangfengxy@sina.cn>
2025-08-19 14:02:38 +0800

    删除废弃的代码

--- blas/waves_test.go
+++ blas/waves_test.go
@@ -1,15 +1,17 @@
 package blas
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 
 	"gitee.com/quant1x/engine/datasource/base"
 	"gitee.com/quant1x/exchange"
+	"gitee.com/quant1x/gox/api"
 )
 
 func TestWaves_basic(t *testing.T) {
-	requiredKLines := 89
+	requiredKLines := 0
 	//requiredKLines = 250
 	code := "sh000001"
 	code = "sz000158"
@@ -17,7 +19,7 @@ func TestWaves_basic(t *testing.T) {
 	date = "2025-06-23"
 	//date = cache.DefaultCanReadDate()
 	list := base.CheckoutKLines(code, date)
-	if len(list) >= requiredKLines {
+	if requiredKLines > 0 && len(list) >= requiredKLines {
 		list = list[len(list)-requiredKLines:]
 	}
 	sample := LoadKLineSample(list)
@@ -25,4 +27,7 @@ func TestWaves_basic(t *testing.T) {
 	securityCode := exchange.CorrectSecurityCode(code)
 	waves := PeaksAndValleys(sample, securityCode)
 	fmt.Println(waves)
+	data, _ := json.Marshal(waves)
+	text := api.Bytes2String(data)
+	fmt.Println(text)
 }
